af384d10a0fa8dee4b729d282392ed39bd80abde
Brendan Moore <brendan.moore@rangle.io>
2018-08-26 13:33:41 -0400

    ✨ Add pages for AM/PM line directions

--- src/components/station-schedule/station-schedule.js
+++ src/components/station-schedule/station-schedule.js
@@ -1,35 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Document, Page } from 'react-pdf/dist/entry.webpack';
 
-import { Box, Hide } from 'rebass';
+import { Box, Hide, Link, Button } from 'rebass';
 
 import { stationsByName } from '../../config/stations';
+import lines from '../../config/lines';
 
 const pdfFilepath = station => 
   `../../assets/pdfs/${stationsByName[station].line}/${stationsByName[station].miniScheduleFilename}`;
 
-const StationSchedule = ({ station }) => (
-  <Box>
-    <Document
-      file={pdfFilepath(station)}
-      >
-      <Hide xs sm md lg>
-        <Page pageNumber={2} scale={1.9}/>
-      </Hide>
-      <Hide xs sm md xl>
-        <Page pageNumber={2} scale={1.65}/>
-      </Hide>
-      <Hide xs sm lg xl>
-        <Page pageNumber={2} scale={1.45}/>
-      </Hide>
-      <Hide xs md lg xl>
-        <Page pageNumber={2} scale={1.15}/>
-      </Hide>
-      <Hide sm md lg xl>
-        <Page pageNumber={2} scale={0.80}/>
-      </Hide>
-    </Document>
-  </Box>
-);
+const schedulePages = station => {
+  const directions = lines[stationsByName[station].line].majorDirections;
+  const pages = [
+    { direction: directions.am, page: stationsByName[station].pages[directions.am] },
+    { direction: directions.pm, page: stationsByName[station].pages[directions.pm] },
+  ];
+
+  // Only return pages if distinct
+  return pages[0].page !== pages[1].page ? pages : [];
+};
+
+class StationSchedule extends Component {
+  constructor() {
+    super();
+    this.state = { currentPage: 2 };
+  }
+
+  setPage = page =>
+    () => this.setState({ currentPage: page });
+
+  render() {
+    const { station } = this.props;
+    const { currentPage } = this.state;
+    return (
+      <Box>
+        <Box>
+          {
+            schedulePages(station).map(p =>
+              <Button
+                key={p.direction}
+                mx={2}
+                my={1}
+                onClick={this.setPage(p.page)}
+              >
+                { p.direction }
+              </Button>
+            )
+          }
+        </Box>
+        <Document
+          file={pdfFilepath(station)}
+          >
+          <Hide xs sm md lg>
+            <Page pageNumber={currentPage} scale={1.9}/>
+          </Hide>
+          <Hide xs sm md xl>
+            <Page pageNumber={currentPage} scale={1.65}/>
+          </Hide>
+          <Hide xs sm lg xl>
+            <Page pageNumber={currentPage} scale={1.45}/>
+          </Hide>
+          <Hide xs md lg xl>
+            <Page pageNumber={currentPage} scale={1.15}/>
+          </Hide>
+          <Hide sm md lg xl>
+            <Page pageNumber={2} scale={0.80}/>
+          </Hide>
+        </Document>
+      </Box>
+    );
+  };
+};
 
 export default StationSchedule;
