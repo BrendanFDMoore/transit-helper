38026644687f93c5024744011562af569bfaf8c5
Brendan Moore <brendan.moore@rangle.io>
2018-08-25 22:14:42 -0400

    ✨ Add PDF schedules instead of images. 🔪 A bit hacky for responsive width... but it works. Limited by the width/scale controls within react-pdf

--- src/components/station-detail/station-detail.js
+++ src/components/station-detail/station-detail.js
@@ -1,11 +1,19 @@
-import React, { Component } from "react";
-import { Fixed, Heading, Modal, Image, Flex, Box, Panel, Link, Text, Close, ButtonCircle } from "rebass";
-import stationConfig from '../map/stationConfig';
+import React, { Component } from 'react';
+import { Fixed, Heading, Modal, Image, Flex, Box, Panel, Link, Text, Close, ButtonCircle, Hide } from 'rebass';
+import { Document, Page } from 'react-pdf/dist/entry.webpack';
+
 import ComingSoon from '../coming-soon/coming-soon';
+
+import { stationsByName } from '../../config/stations';
+import lines from '../../config/lines';
+import stationConfig from '../map/stationConfig';
 
 // This page does not work, currently.
 const getDeparturesUrl = stationCode =>
   `https://www.gotransit.com/en/trip-planning/scheduled-departures/station?stationCode=${stationCode}`;
+
+const pdfFilepath = station => 
+  `../../assets/pdfs/${stationsByName[station].line}/${stationsByName[station].miniScheduleFilename}`;
 
 const StationDetail = ({ station, onModalClick }) => (
   <div>
@@ -35,7 +43,7 @@ const StationDetail = ({ station, onModalClick }) => (
             <Box flex={1} py={1}>
                <Text textAlign='center'>{stationConfig[station].name}</Text>
             </Box>
-            <Box xflex={0} xalignSelf="flex-start">
+            <Box>
               <ButtonCircle
                 bg='white'
                 px={1}
@@ -49,19 +57,50 @@ const StationDetail = ({ station, onModalClick }) => (
             </Box>
           </Flex>
         </Panel.Header>
-        <Box p={3}>
-          {
-            stationConfig[station].miniSchedule ? 
-            <Image
-              src={stationConfig[station].miniSchedule}
-            />
-            : <ComingSoon text="Mini schedule" />
-          }
-        </Box>
+        <Flex
+          justifyContent="space-around"
+        >
+          <Box p={3}>
+            <Hide xs sm md lg>
+              <Document
+                file={pdfFilepath(station)}
+                >
+                <Page pageNumber={2} scale={1.9}/>
+              </Document>
+            </Hide>
+            <Hide xs sm md xl>
+              <Document
+                file={pdfFilepath(station)}
+                >
+                <Page pageNumber={2} scale={1.65}/>
+              </Document>
+            </Hide>
+            <Hide xs sm lg xl>
+              <Document
+                file={pdfFilepath(station)}
+                >
+                <Page pageNumber={2} scale={1.45}/>
+              </Document>
+            </Hide>
+            <Hide xs md lg xl>
+              <Document
+                file={pdfFilepath(station)}
+                >
+                <Page pageNumber={2} scale={1.15}/>
+              </Document>
+            </Hide>
+            <Hide sm md lg xl>
+              <Document
+                file={pdfFilepath(station)}
+                >
+                <Page pageNumber={2} scale={0.80}/>
+              </Document>
+            </Hide>
+          </Box>
+        </Flex>
         <Panel.Footer
           color='black'
           borderTop={0}
-          xborderColor='green'
         >
           <Flex>
             <Box flex={1}>
